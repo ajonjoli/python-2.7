93c7a9ae1e64b21ece76d65a136184c2e8dcde80
Luis Ortiz Chan <myluisortiz@gmail.com>
2015-11-04 18:04:19 -0600

    Create miModulo.py

---
+++ miModulo.py
@@ -0,0 +1,16 @@
+﻿#mandado a llamar por Python manual.py en la sección Módulos y paquetes
+def miFuncion():
+	print "una función"
+
+def miOtraFuncion():
+	print "otra función"
+	
+class miClase:
+	def __init__(self):
+		print "una clase"
+
+print "un módulo"	#la primera ejecución de alguna función de este módulo ejecuta esto
+
+if __name__=='__main__':
+#atributo name: si es importado es el nombre del archivo, si es ejecución directa es main
+	print 'Se mostrará si no es importado'
